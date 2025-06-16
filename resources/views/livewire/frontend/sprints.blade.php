0210d9c1315abb727c8a2874e83a3344906c1002
Robert Dam <info@rd-media.org>
2025-06-16 15:12:06 +0200

    Show message when no results. Set default element.

--- resources/views/livewire/frontend/sprints.blade.php
+++ resources/views/livewire/frontend/sprints.blade.php
@@ -7,7 +7,7 @@ use Livewire\Volt\Component;
 
 new #[Layout('components.layouts.app.frontend')] class extends Component {
 
-	public string $element = '';
+	public string $element = 'SKEELER_SPRINT_1';
 
 	public function getListeners(): array
 	{
@@ -53,42 +53,48 @@ new #[Layout('components.layouts.app.frontend')] class extends Component {
 		<flux:select.option value="RUNNING_SPRINT_2">Hardlopen Sprint 2</flux:select.option>
 	</flux:select>
 
-	@foreach($sprints as $index => $sprint)
-		<div class="w-full flex mt-3">
-			<div class="w-10 h-10 mt-3 mr-4 flex-shrink-0 {{$sprint->completed_at ? 'bg-green-800' : ''}} border border-b-gray-600 dark:border-white rounded-md flex items-center justify-center text-lg font-semibold">
-				{{str_pad($index+1, 2, '0', STR_PAD_LEFT)}}
-			</div>
-			<div class="flex-1 text-sm">
-				<div class="w-full h-6 mt-1">
-					@if($sprint->athlete_one)
-						<div class="float-right">
-							@if($sprint->athlete_one_timer_time)
-								<flux:badge size="sm" color="{{$sprint->athlete_one_timer_time < $sprint->athlete_two_timer_time ? 'green' : 'grey'}}">
-									{{ $sprint->athlete_one_timer_time ?number_format($sprint->athlete_one_timer_time, 2, '.', '') : null}}
-								</flux:badge>
-							@endif
-						</div>
-						{{$sprint->athlete_one->name}} <span class="text-xs"></span>
-					@else
-						-/-
-					@endif
-				</div>
-				<flux:separator class="mb-1 mt-1"/>
-				<div class="w-full h-6 ">
-					@if($sprint->athlete_two)
-						<div class="float-right">
-							@if($sprint->athlete_two_timer_time)
-								<flux:badge size="sm" color="{{$sprint->athlete_two_timer_time < $sprint->athlete_one_timer_time ? 'lime' : 'grey'}}">
-									{{ $sprint->athlete_two_timer_time ?number_format($sprint->athlete_two_timer_time, 2, '.', '') : null}}
-								</flux:badge>
-							@endif
-						</div>
-						{{$sprint->athlete_two->name}} <span class="text-xs"></span>
-					@else
-						-/-
-					@endif
-				</div>
-			</div>
+	@if($sprints->count() === 0)
+		<div class="text-center text-gray-500 dark:text-gray-300">
+			Geen sprints gevonden voor dit onderdeel. Selecteer de juiste sprint om de resultaten te bekijken.
 		</div>
-	@endforeach
+	@else
+		@foreach($sprints as $index => $sprint)
+			<div class="w-full flex mt-3">
+				<div class="w-10 h-10 mt-3 mr-4 flex-shrink-0 {{$sprint->completed_at ? 'bg-green-800' : ''}} border border-b-gray-600 dark:border-white rounded-md flex items-center justify-center text-lg font-semibold">
+					{{str_pad($index+1, 2, '0', STR_PAD_LEFT)}}
+				</div>
+				<div class="flex-1 text-sm">
+					<div class="w-full h-6 mt-1">
+						@if($sprint->athlete_one)
+							<div class="float-right">
+								@if($sprint->athlete_one_timer_time)
+									<flux:badge size="sm" color="{{$sprint->athlete_one_timer_time < $sprint->athlete_two_timer_time ? 'green' : 'grey'}}">
+										{{ $sprint->athlete_one_timer_time ?number_format($sprint->athlete_one_timer_time, 2, '.', '') : null}}
+									</flux:badge>
+								@endif
+							</div>
+							{{$sprint->athlete_one->name}} <span class="text-xs"></span>
+						@else
+							-/-
+						@endif
+					</div>
+					<flux:separator class="mb-1 mt-1"/>
+					<div class="w-full h-6 ">
+						@if($sprint->athlete_two)
+							<div class="float-right">
+								@if($sprint->athlete_two_timer_time)
+									<flux:badge size="sm" color="{{$sprint->athlete_two_timer_time < $sprint->athlete_one_timer_time ? 'lime' : 'grey'}}">
+										{{ $sprint->athlete_two_timer_time ?number_format($sprint->athlete_two_timer_time, 2, '.', '') : null}}
+									</flux:badge>
+								@endif
+							</div>
+							{{$sprint->athlete_two->name}} <span class="text-xs"></span>
+						@else
+							-/-
+						@endif
+					</div>
+				</div>
+			</div>
+		@endforeach
+	@endif
 </div>
